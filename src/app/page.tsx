47da05ea4c3f21e3205c8468bcadd496405bcaaf
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2024-09-25 23:01:35 -0400

    pagina login
    atividade 1 sábado letivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
+'use client'
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-     <h1>Ai papai</h1>
-    </div>
-  );
+  const [user, setUser] = useState(true)
+  const route = useRouter()
+  if (user) {
+    return (
+      <div className={styles.page}>
+        <h1>Logins</h1>
+        <h2>e-mail</h2>
+        <input></input>
+        <h2>senha</h2>
+        <input></input>
+        <button onClick={() =>
+          setUser(false)}>Fazer login
+        </button>
+        {/* <button onClick={() => route.push('/login')}>Butão</button> */}
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className={styles.page}>
+        <h1>Erro ao realizar o login</h1>
+        <button onClick={() =>
+          setUser(true)}>Voltar para a pagina inicial
+        </button>
+      </div>
+    );
+  }
+
 }
