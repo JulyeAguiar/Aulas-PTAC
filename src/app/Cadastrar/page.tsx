11ff2a3b8158b501c85e553c884e85cb4a6ac6a9
JulyeAguiar <131816932+JulyeAguiar@users.noreply.github.com>
2024-11-11 23:00:10 -0400

    tentativa para linkagem da página cadastrar

--- src/app/Cadastrar/page.tsx
+++ src/app/Cadastrar/page.tsx
@@ -1,13 +1,17 @@
 
 'use client'
-import { useState } from "react"
+import { useState, useEffect, FormEvent } from "react"
 import Usuario from "../../../interfaces/usuario"
 import { setCookie, parseCookies } from 'nookies'
+import { ApiURL } from "../../../config"
+import { useRouter } from "next/navigation"
 
 const Cadastrar = () => {
   const [nome, setNome] = useState<string>()
   const [senha, setSenha] = useState<string>()
   const [email, setEmail] = useState<string>()
+  const [error, setError] = useState("");
+  const router = useRouter()
   const [usuario, setUsuario] = useState<Usuario>({
     nome: '',
     email: '',
@@ -39,9 +43,47 @@ const Cadastrar = () => {
     console.log(usuario)
   }
 
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+
+      const response = await fetch(`${ApiURL}/auth/cadastro`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(usuario)
+      })
+
+      if (response) {
+
+        const data = await response.json();
+        const { erro, mensagem }= data
+
+        console.log(data)
+
+        if (erro) {
+          setError(mensagem)
+          console.log("tá errado aqui zé")
+        }
+        else{
+          router.push('/cadastrar')
+        }
+        
+      }
+    } catch (error) {
+      console.error('Erro na requisicao', error)
+    }
+
+    console.log('Email:', email);
+    console.log('Senha:', senha);
+    console.log('Nome:', nome);
+  };
+
   return (
 
     <div className="register-container">
+          <form onSubmit={handleSubmit}>
       <div className="register-form">
 
         <h1 className="register-title">Cadastrar</h1>
@@ -55,9 +97,12 @@ const Cadastrar = () => {
         <label htmlFor="senha" className="register-label">Senha:</label>
         <input id="senha" type="password" value={senha} onChange={(e) => alterar_Senha(e.target.value)} className="register-input" />
 
-        <button className="register-button">Cadastrar</button>
+        <button className="register-button" onSubmit={handleSubmit}>Cadastrar</button>
+
+        <a href="/login">Fazer login</a>
 
       </div>
+</form>
     </div>
   )
 }
